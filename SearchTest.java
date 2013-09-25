3df17a246a06f871df981f408ccceba6fd4f251b
Narkidae <connorbtaylor@gmail.com>
2013-09-25 16:47:34 -0500

    Implemented Expected Values

--- SearchTest.java
+++ SearchTest.java
@@ -11,12 +11,11 @@ public class SearchTest {
 	
 	public void test() throws IOException {
 		//Make a collection from a file
-		PaperCollection testCollection = new PaperCollection("filename");
+		PaperCollection testCollection = new PaperCollection("papers.txt");
 		
 		//Test to see if you can find the right information
-		Assert.assertEquals("expected",testCollection.search("author1"));
-		Assert.assertEquals("expected",testCollection.search("title2"));
-		Assert.assertEquals("expected",testCollection.search("DOI"));
+		Assert.assertEquals("Conference Paper\nAuthorLastName1, AuthorFirstName1; AuthorLastName2, AuthorFirstName2\nPaperName1\nSerialTitle1\n137-144\nJuly 2012\nhttp://dx.doi.org/10.7551/978-0-262-31050-5-ch020",testCollection.search("PaperName1"));
+		Assert.assertEquals("Journal Article\nAuthorLastName7, AuthorFirstName7; AuthorLastName8, AuthorFirstName8\nPaperName4\nSerialTitle4\n30(2):115-634\nOctober 2010",testCollection.search("SerialTitle4"));
 		
 	}
 
