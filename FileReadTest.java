766f2d605c3b677efe52d98529939241b844d360
Narkidae <connorbtaylor@gmail.com>
2013-09-25 14:00:47 -0500

    Update FileReadTest.java

--- FileReadTest.java
+++ FileReadTest.java
@@ -7,10 +7,11 @@ public class FileReadTest {
 
 	@Test
 	public void test() {
+		//Filename containing 4 test papers
 		String filepath = "filepath";
 		PaperCollection testCollection = new PaperCollection(filepath);
 		
-		fail("Not yet implemented.");
+		assert.equals(testCollection.size(), 4);
 	}
 
 }
