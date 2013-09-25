8ed3c3ed927bc598f1dcc12aa1fd4f94078aa575
Narkidae <connorbtaylor@gmail.com>
2013-09-25 16:24:18 -0500

    Implemented test

--- SortTest.java
+++ SortTest.java
@@ -1,13 +1,29 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 
 public class SortTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws IOException {
+		PaperCollection testCollection = new PaperCollection("filename");
+		
+		testCollection.Sort("BI");
+		Assert.assertEquals("expected", testCollection.toString());
+		testCollection.Sort("AN");
+		Assert.assertEquals("expected", testCollection.toString());
+		testCollection.Sort("PT");
+		Assert.assertEquals("expected", testCollection.toString());
+		testCollection.Sort("ST");
+		Assert.assertEquals("expected", testCollection.toString());
+		testCollection.Sort("CH");
+		Assert.assertEquals("expected", testCollection.toString());
+		testCollection.Sort("R");
+		Assert.assertFalse("expected".equals(testCollection.toString()));
 	}
 
 }
