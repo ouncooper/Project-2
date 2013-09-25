8f2722d39698f63a80b1eaaa2a8935f29091ec09
Narkidae <connorbtaylor@gmail.com>
2013-09-25 16:16:23 -0500

    Implemented test

--- SearchTest.java
+++ SearchTest.java
@@ -1,13 +1,23 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 
 public class SearchTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented.");
+	
+	public void test() throws IOException {
+		//Make a collection from a file
+		PaperCollection testCollection = new PaperCollection("filename");
+		
+		//Test to see if you can find the right information
+		Assert.assertEquals("expected",testCollection.search("author1"));
+		Assert.assertEquals("expected",testCollection.search("title2"));
+		Assert.assertEquals("expected",testCollection.search("DOI"));
+		
 	}
 
 }
