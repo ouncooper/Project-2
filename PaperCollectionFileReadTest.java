b97afe5d65bc1e41594d25b429626e211994efc0
Narkidae <connorbtaylor@gmail.com>
2013-10-02 18:27:31 -0500

    READ TEST - FINAL

---
+++ PaperCollectionFileReadTest.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class PaperCollectionFileReadTest {
+
+	@Test
+	public void test() {
+		//Filename containing exactly 4 test papers
+		String filepath = "papers.txt";
+
+		PaperCollection testCollection = null;
+		try {
+			testCollection = new PaperCollection(filepath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//See if there are exactly 4 papers in the collection
+		Assert.assertEquals(testCollection.size(), 4);
+		
+	}	
+}
