da65af2cd2b4f2e3b43b59be91c00ccd40baa1af
Narkidae <connorbtaylor@gmail.com>
2013-10-02 16:09:50 -0500

    Changed JavaDoc and got rid of unused constructor

--- JournalArticle.java
+++ JournalArticle.java
@@ -10,30 +10,7 @@ public class JournalArticle extends Paper
 	
 	/**
 	 * Inherits from the Paper class.
-	 * Constructs the object with no DOI given.
-	 * --Possibly may be removed due to a lack of use since the current method passes a null DOI to the other constructor.
-	 * @param newName Name of the paper ("Journal Article").
-	 * @param newAuthors Authors of the paper in the format Last, First Middle; Last, First Middle; etc.
-	 * @param newTitle Title of the paper.
-	 * @param newSerialTitle Serial title of the paper.
-	 * @param newDate Date the paper was published.
-	 * @param newVolumeIssuePageRange Volume, issue, and page range specified in the file.  Split into two variables.
-	 **/
-	public JournalArticle(String newName, String newAuthors, String newTitle, String newSerialTitle, String newVolumeIssuePageRange,  String newDate)
-	{
-		setName(newName);
-		setName(newAuthors);
-		setTitle(newTitle);
-		setSerialTitle(newSerialTitle);
-		volumeIssue = newVolumeIssuePageRange.split(":")[0];
-		setPageRange(newVolumeIssuePageRange.split(":")[1]);
-		setDate(newDate);
-		setDOI(null);
-	}
-	
-	/**
-	 * Inherits from the Paper class.
-	 * Preferred constructor for the object with a DOI given in the parameters.  
+	 * Creates a JournalArticle Object
 	 * @param newName Name of the paper ("Journal Article").
 	 * @param newAuthors Authors of the paper in the format Last, First Middle; Last, First Middle; etc.
 	 * @param newTitle Title of the paper.
@@ -63,7 +40,7 @@ public class JournalArticle extends Paper
 	}
 	
 	/**
-	 * Converts the object to a string separated by " \ "
+	 * Converts the object to a string separated by " // "
 	 */
 	public String toString()
 	{
